6d52aae9837298123118307e84dd2c7f992244e0
General-Monish <monissam28@gmail.com>
2024-05-17 02:50:07 +0530

    finished prototype3

--- Scripts/Prototype3/Player3.cs
+++ Scripts/Prototype3/Player3.cs
@@ -15,11 +15,19 @@ public class Player3 : MonoBehaviour
     bool isGround = true;
     Animator animator;
 
+    AudioSource playAudio;
+
     [SerializeField]
     private ParticleSystem explosionPrefab;
+    [SerializeField]
+    private ParticleSystem dirtParticlePrefab;
+
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip crashSound;
     // Start is called before the first frame update
     void Start()
     {
+        playAudio = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
@@ -30,9 +38,11 @@ public class Player3 : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGround && !gameOver)
         {
+            playAudio.PlayOneShot(jumpSound, 1f);
             animator.SetTrigger("Jump_trig");
             rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
             isGround = false;
+            dirtParticlePrefab.Play();
         }
     }
 
@@ -40,14 +50,18 @@ public class Player3 : MonoBehaviour
     {
         if (collision.collider.CompareTag("Ground"))
         {
+            dirtParticlePrefab.Play();
             isGround = true;
-        }else if (collision.collider.CompareTag("obs"))
+        }
+        else if (collision.collider.CompareTag("obs"))
         {
+            playAudio.PlayOneShot(crashSound, 1f);
             Debug.Log("Game Over");
-            gameOver=true;
+            gameOver = true;
             animator.SetBool("Death_b", true);
             animator.SetInteger("DeathType_int", 1);
             explosionPrefab.Play();
+            dirtParticlePrefab.Stop();
         }
     }
 }
