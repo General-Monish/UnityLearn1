2411fb6b9e7931d5ff40efd2185a003834144a2c
General-Monish <monissam28@gmail.com>
2025-03-14 07:01:42 +0530

    Finished Car Assignment

--- Scripts/Prototype1/PlayerController.cs
+++ Scripts/Prototype1/PlayerController.cs
@@ -1,26 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement; // Required for UI Pointer Events
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 20f;
+    [SerializeField] float speed;
+    [SerializeField] float rpm;
+    [SerializeField] float HorsePower;
     float horizontalInput;
-    float ForwardInput;
+    float forwardInput;
     [SerializeField] float turnSpeed = 45f;
-    // Start is called before the first frame update
+    [SerializeField] GameObject centerOfMass;
+    Rigidbody rb;
+    [SerializeField] TextMeshProUGUI SpeedText;
+    [SerializeField] TextMeshProUGUI RpmText;
+    bool isGrounded;
+    bool isGameOver;
+    [SerializeField] List<WheelCollider> ALLwheelColliders;
+    int wheelsOnGround;
+
+    bool moveForward = false;
+    bool moveBackward = false;
+    bool turnLeft = false;
+    bool turnRight = false;
+
+    [SerializeField] Button restartBtn;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.transform.position;
+        restartBtn.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        ForwardInput = Input.GetAxis("Vertical");
+        // Apply movement based on button state
+        forwardInput = moveForward ? 1 : (moveBackward ? -1 : 0);
+        horizontalInput = turnRight ? 1 : (turnLeft ? -1 : 0);
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * ForwardInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        if (IsGrounded())
+        {
+            rb.AddRelativeForce(Vector3.forward * forwardInput * HorsePower);
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+            speed = Mathf.RoundToInt(rb.velocity.magnitude * 2.237f);  // Convert to mps
+            SpeedText.SetText("Speed: " + speed + " mps");
+
+            rpm = (speed % 30) * 40;
+            RpmText.SetText("RPM: " + rpm);
+        }
+        GameOver();
+    }
+
+    bool IsGrounded()
+    {
+        wheelsOnGround = 0;
+
+        foreach (WheelCollider wheel in ALLwheelColliders)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        return wheelsOnGround == 4;
+    }
+
+    // UI Pointer Down (Start Moving)
+    public void OnMoveForwardPress() { moveForward = true; }
+    public void OnMoveBackwardPress() { moveBackward = true; }
+    public void OnTurnLeftPress() { turnLeft = true; }
+    public void OnTurnRightPress() { turnRight = true; }
+
+    // UI Pointer Up (Stop Moving)
+    public void OnMoveForwardRelease() { moveForward = false; }
+    public void OnMoveBackwardRelease() { moveBackward = false; }
+    public void OnTurnLeftRelease() { turnLeft = false; }
+    public void OnTurnRightRelease() { turnRight = false; }
+
+    void GameOver()
+    {
+        if(transform.position.y <  -10)
+        {
+            isGameOver = true;
+            RestartBtn();
+        }
+        else
+        {
+            isGameOver = false;
+        }
+    }
+
+    void RestartBtn()
+    {
+        restartBtn.gameObject.SetActive(true);
+        restartBtn.onClick.AddListener(()=>{
+            SceneManager.LoadScene("Car");
+        });
     }
 }
